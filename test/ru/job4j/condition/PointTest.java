580c6b7b6557a8d2a957587de0b5d7634944c9f2
petrs <sychayev.petr>
2020-09-17 19:50:45 +0300

    4.6.2. Switch - matcher

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -4,9 +4,9 @@ package ru.job4j.condition;
 //import org.junit.Test;
 //
 //import static org.junit.Assert.*;
-//
-//public class PointTest {
-//
+
+public class PointTest {
+
 //    @Test
 //    public void distanceFirst() {
 //        int x1 = 0;
@@ -14,7 +14,7 @@ package ru.job4j.condition;
 //        int x2 = 0;
 //        int y2 = 2;
 //        double expected = 2.0;
-//        double out = Point.main (x1, y1, x2, y2);
+//        double out = Point. (x1, y1, x2, y2);
 //        Assert.assertEquals(expected, out, 0.01);
 //    }
-//}
+}
