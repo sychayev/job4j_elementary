af863a8312618fc73e857ed25cca6e56ff1bda8f
petrs <sychayev.petr>
2020-09-17 20:05:26 +0300

    4.6.2. Switch - matcher

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class Point {
     private int x;
     private int y;
-
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
