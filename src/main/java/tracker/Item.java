d5c022ee96ecd68a46f033a13c03075793147f40
petrs <sychayev.petr>
2020-09-18 21:05:11 +0300

    2. Перегрузка конструктора

---
+++ src/main/java/tracker/Item.java
@@ -0,0 +1,20 @@
+package tracker;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item(){
+
+    }
+    public Item(int id){
+
+    }
+    public Item(int id ,String name){
+
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
