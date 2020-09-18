ef2dc8b91143288ae83e6f4fe655868f721eaa15
petrs <sychayev.petr>
2020-09-18 18:59:00 +0300

    4. Переопределение

--- src/main/java/inheritance/JSONReport.java
+++ src/main/java/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 public class JSONReport extends TextReport {
+
     public static void main(String[] args) {
         TextReport report = new TextReport();
         String text = report.generate("name","body");
--- src/main/java/inheritance/TextReport.java
+++ src/main/java/inheritance/TextReport.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 public class TextReport {
+
     public String generate(String name, String body) {
         return name + " : " + "name" + "," + System.lineSeparator()
                 + body + " : " + "body";
