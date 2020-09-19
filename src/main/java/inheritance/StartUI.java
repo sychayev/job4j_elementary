4ae4d897dfc480d610f675b93f9b7e137d7175c4
petrs <sychayev.petr>
2020-09-19 11:14:35 +0300

    7. Date. Отображение даты.

--- src/main/java/inheritance/StartUI.java
+++ src/main/java/inheritance/StartUI.java
@@ -2,9 +2,12 @@ package inheritance;
 
 import tracker.Item;
 
+import java.time.format.DateTimeFormatter;
+
 public class StartUI extends Item {
     public static void main(String[] args) {
         Item i = new Item();
         System.out.println(i.getCreated());
+        i.Formatter();
     }
 }
--- src/main/java/tracker/Item.java
+++ src/main/java/tracker/Item.java
@@ -1,30 +1,42 @@
 package tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created;
-    public void Time(LocalDateTime created){
+
+    public void Time(LocalDateTime created) {
         this.created = created;
     }
 
-    public  LocalDateTime getCreated() {
+    public void Formatter() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern
+                ("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String feedbackDateTimeFormatter = dateTime.format(formatter);
+        System.out.println(feedbackDateTimeFormatter);
+    }
 
+    public LocalDateTime getCreated() {
         return created = LocalDateTime.now();
     }
 
-    public Item(){
+    public Item() {
 
     }
-    public Item(int id){
+
+    public Item(int id) {
         this.id = id;
 
     }
-    public Item(int id ,String name){
+
+    public Item(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
 
 }
