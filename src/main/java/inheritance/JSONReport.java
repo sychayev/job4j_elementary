03f0a40b642c33ba49423ddb6c9a8ab47a7f1807
petrs <sychayev.petr>
2020-09-20 11:57:53 +0300

    5. Аннотация @Override

--- src/main/java/inheritance/JSONReport.java
+++ src/main/java/inheritance/JSONReport.java
@@ -1,10 +1,9 @@
 package inheritance;
 
 public class JSONReport extends TextReport {
-
     public static void main(String[] args) {
         TextReport report = new TextReport();
-        String text = report.generate("name","body");
+        String text = report.generate("name", "body");
         System.out.println(text);
     }
 
--- src/main/java/inheritance/TextReport.java
+++ src/main/java/inheritance/TextReport.java
@@ -1,8 +1,9 @@
 package inheritance;
 
 public class TextReport {
+    @Override
+    public String genrate(String name, String body) {
 
-    public String generate(String name, String body) {
         return name + " : " + "name" + "," + System.lineSeparator()
                 + body + " : " + "body";
     }
