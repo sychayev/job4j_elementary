c0d83f21345843efa2d146c67ce6aed48d233e97
petrs <sychayev.petr>
2020-09-20 10:52:09 +0300

    3. Перегрузить метод max для трех чисел

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,29 +3,14 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int first, int second) {
         int result = 0;
-        if (first > second) {
-            return result = first;
-        }
-        if (second > first) {
-            return result = second;
-        }
-        if (second == first) {
-            return result = first;
-        }
+        result = (first > second) ? first : second;
         return result;
     }
 
     public static int max(int first, int second, int third) {
         int result = first;
-        if (first > second && first > third) {
-            return first;
-        }
-        if (second > first && second > third) {
-            return second;
-        }
-        if (third > first && third > second) {
-            return third;
-        }
+        result = (second > first) && (second > third) ? second : third;
+        result = (third > first) && (third > second) ? third : first;
         int temp = max(first, second);
         return max(temp, third);
 
@@ -33,26 +18,16 @@ public class Max {
 
     public static int max(int first, int second, int third, int fourth) {
         int result = 0;
-        if (first > second && first > third && first > fourth) {
-            return result = first;
-        }
-        if (second > first && second > third && second > fourth) {
-            return result = second;
-        }
-        if (third > first && third > second && third > fourth) {
-            return result = third;
-        }
-        if (fourth > first && fourth > second && fourth > third) {
-            return result = fourth;
-        }
+        result = (fourth > first) && (fourth > third)
+                && (fourth > second) ? fourth : third;
         int temp = max(first, second, third);
         return max(temp, fourth);
 
     }
 
     public static void main(String[] args) {
-        System.out.println(Max.max(10,5));
-        System.out.println(Max.max(1,5,2));
-        System.out.println(Max.max(10,5,3,11));
+        System.out.println(Max.max(10, 5));
+        System.out.println(Max.max(1, 5, 2));
+        System.out.println(Max.max(10, 5, 3, 11));
     }
 }
