b728dff9253396bbabcf5d0c51d58f225a2973f8
petrs <sychayev.petr>
2020-09-25 19:13:06 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/io/Matches.java
+++ src/main/java/io/Matches.java
@@ -7,29 +7,23 @@ public class Matches {
         Scanner in = new Scanner(System.in);
         boolean run = true;
         int matches = 11;
-        int count1 = 0;
-        int count2 = 0;
+        int count = 0;
         boolean player = true;
-        while (run){
+        while (matches > 0) {
             String name = player ? "gamer1" : "gamer2";
             System.out.println("Enter your number from 1 to 3 - " + name);
-            count1 = in.nextInt();
-            if (count1 < 1 || count1 > 3) {
-                System.out.println("You entered not right number ");
+            count = in.nextInt();
+            if (count > 0 && count < 4) {
+                matches = matches - count;
+                player = !player;
+                System.out.println(matches);
+            }  else {
+                System.out.println("You entered not right number!!");
+            }
+           if (matches == 0) {
+                System.out.println( "You won.Congratulation!!");
             }
 
-            String name1 = player ? "gamer2" : "gamer1";
-            System.out.println("Enter your number from 1 to 3 " + name1);
-            count2 = in.nextInt();
-            if (count2 < 1 || count2 > 3) {
-                System.out.println("You entered not right number ");
-            }
-            int sum = count1 + count2;
-            int sub = matches - sum;
-            System.out.println(sub);
-            if (sub == 0) {
-                run = false;
-            }
         }
     }
 }
