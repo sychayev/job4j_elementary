2889b8c412c1f2ccfd6417c3155e68655db6c062
petrs <sychayev.petr>
2020-09-27 11:15:54 +0300

    6. Сдача в кофе машине

--- src/main/java/ru/job4j/sort/Machine.java
+++ src/main/java/ru/job4j/sort/Machine.java
@@ -1,26 +1,30 @@
 package ru.job4j.sort;
 
 import java.util.Arrays;
+
 public class Machine {
 
     public static int[] change(int money, int price) {
         int[] COINS = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        System.out.println( size = money - price);
-        for(int i = 0;i <COINS.length;i++){
-            while(size > COINS[i]){
-             int nom =  size - COINS[i];
+        int balance = 0;
+        int nom = 0;
+        System.out.println(balance = money - price);
+        for (int i = 0; i < COINS.length; i++) {
+
+            while (balance >= COINS[i]) {
+                nom = balance -= COINS[i];
+                size++;
+                System.out.println(nom);
             }
-            System.out.println(COINS[i]);
-//            System.out.println(i);
         }
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(rsl, nom);
     }
 
     public static void main(String[] args) {
-       int mas1[] =  Machine.change(100,100);
-       int mas2[] =  Machine.change(50,35);
+        int mas1[] = Machine.change(100, 100);
+        int mas2[] = Machine.change(50, 35);
 
     }
 }
